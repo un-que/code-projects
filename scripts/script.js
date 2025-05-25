e5a418d272aceb67de92e38c25377a99ed33b0b0
un-que <vialine@duck.com>
2025-05-25 17:14:46 +0200

    Нифига не понятно, JS штука крутая но надо в ней разобраться.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,25 @@
-function change() {
-  document.getElementById("name").innerHTML = "nigga",
-  document.getElementById("name").style.color = "black"
+document.addEventListener("DOMContentLoaded", ()=> {init()})
+
+function init() {
+  if(localStorage.getItem('theme')) {
+    document.documentElement.setAttribute("theme", "dark");
+  }
+  else {
+    document.documentElement.removeAttribute("theme");
+  }
 }
+
+const toggleBtn = document.querySelector("#button");
+
+function run() {
+  if(document.documentElement.hasAttribute("theme")){
+    document.documentElement.removeAttribute("theme");
+    localStorage.removeItem('theme');
+  }
+  else{
+    document.documentElement.setAttribute("theme", "dark");
+    localStorage.setItem('theme', 1);
+  }
+}
+
+toggleBtn.addEventListener("click", run());
